5317880005f83201786ba9fa70129f5d5caa8f64
minna <yakubuaminat94@gmail.com>
2022-10-03 09:20:55 +0100

    chore: react router setup

---
+++ src/components/RouterTree.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+const RouterTree = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default RouterTree;
